fc69087407b55a2ea8e899a5af17d10fcbd6f073
mitcmurph <mitcmurph@gmail.com.au>
2021-03-15 00:07:58 +1100

    Add retrieval of commandline args With basic text object to render args

---
+++ experimention/frontend-unity/unity/Assets/TextScript.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string[] arguments = Environment.GetCommandLineArgs();
+
+
+        GameObject newGO = new GameObject("myTextGO");
+        newGO.transform.SetParent(this.transform);
+        newGO.transform.localPosition = Vector3.zero;
+
+        Text myText = newGO.AddComponent<Text>();
+        myText.text = arguments[1];
+
+        Font ArialFont = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
+        myText.font = ArialFont;
+        myText.material = ArialFont.material;
+        myText.rectTransform.sizeDelta = new Vector2(300, 300);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

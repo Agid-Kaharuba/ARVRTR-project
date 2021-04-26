a9d084533d0223e5b45838c36125fa660f0db734
Calvin Dong <calvin.dong156@gmail.com>
2021-04-27 02:16:39 +1000

    bottle detects when it is facing burger board stack

--- unity/XRTProject/Assets/sauce_detector.cs
+++ unity/XRTProject/Assets/sauce_detector.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class sauce_detector : MonoBehaviour
-{
+{   
+    private string stackName = "BurgerBoard";  
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +18,17 @@ public class sauce_detector : MonoBehaviour
         RaycastHit hit;
         Vector3 forward = transform.TransformDirection(Vector3.forward)*1;
         //Ray
-        if (Physics.Raycast(transform.position, forward, out hit))
+        if (Physics.Raycast(transform.position, forward, out hit, 2))
         {
             Debug.DrawRay(transform.position, forward, Color.green);
-            Debug.Log(hit);
-            Debug.Log("Did Hit");
+            if (hit.collider.name.Contains(stackName)){
+                Debug.Log("hitting " + hit.collider.name);
+            }
+            /*if (hit.collider.name == "hittingStackCollider")
+            {
+                Debug.Log("hitting" + hit.collider.name);
+            }*/
+            //Debug.Log("Did Hit");
         }
         
     }
